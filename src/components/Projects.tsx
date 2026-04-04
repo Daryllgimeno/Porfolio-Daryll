85b84f1caa6dee492e2026116eaed1db8aacbdfd
Daryll Gimeno <75570103+Daryllgimeno@users.noreply.github.com>
2026-04-04 18:42:00 +0800

    add

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,10 +5,13 @@ const Projects = () => {
 
       <p><strong>Matuturo tutor finder</strong><br />
       A web-based reviewer for students,  Built using php and mysql</p>
+      <a href="https://github.com/Daryllgimeno/Matuturo_Tutor_Finder">Repository</a>
 
       <p><strong>Agriculture inventory system</strong><br />
      A system use for poultry inventory, Built using Vue, php and laravel.
      </p>
+     <a href="https://github.com/Daryllgimeno/Agriculture-inventory-system">Repository</a>
+
     </div>
   );
 };
